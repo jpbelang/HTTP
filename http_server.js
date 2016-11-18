615e423f556ba75bfece5b22a499df1f9049eef1
Jean-Philippe Belanger <jean-philippe.belanger@ericsson.com>
2016-11-18 14:48:45 -0500

    Intermediaire
    Change-Id: I105cf8a0fcccc927febbe710045298aded9ffac9

--- http_server.js
+++ http_server.js
@@ -4,16 +4,48 @@
 
 
 var http = require('http');
+var mysql = require('mysql');
 
-const PORT=8080;
+const PORT = 8080;
 
-function handleRequest(request, response){
-    response.end('It Works!! Path Hit: ' + request.url);
+var req = {
+    "professeur": "select * from professeur where id = ?",
+    "etudiant": "select * from etudiant where id = ?"
+};
+
+function handleRequest(request, response) {
+
+    var url = request.url;
+    var tid = url.split("/")[1];
+    var profid = url.split("/")[2];
+
+    var connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'ebeljea',
+        password: '',
+        database: 'MonApplication'
+    });
+    connection.connect(function (err) {
+
+        if (err) throw err;
+        connection.query(
+            {
+                sql: req[tid],
+                values: [profid]
+            },
+            function (err, rows) {
+                if (err) throw err;
+
+                response.end(JSON.stringify(rows));
+                connection.end();
+            });
+
+    });
 }
 
 var server = http.createServer(handleRequest);
 
-server.listen(PORT, function(){
+server.listen(PORT, function () {
 
     console.log("Server listening on: http://localhost:%s", PORT);
 });
